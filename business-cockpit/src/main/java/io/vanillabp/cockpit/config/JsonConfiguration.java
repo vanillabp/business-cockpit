820bdf6a4b5b45eca42227d38589eeaa0c6cf405
Stephan Pelikan <stephan.pelikan@phactum.at>
2023-04-24 19:42:52 +0200

    Set JSON serialization as it was before enabling webflux

---
+++ business-cockpit/src/main/java/io/vanillabp/cockpit/config/JsonConfiguration.java
@@ -0,0 +1,77 @@
+package io.vanillabp.cockpit.config;
+
+import org.springframework.boot.autoconfigure.jackson.Jackson2ObjectMapperBuilderCustomizer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.codec.ServerCodecConfigurer;
+import org.springframework.http.codec.json.Jackson2JsonDecoder;
+import org.springframework.http.codec.json.Jackson2JsonEncoder;
+import org.springframework.web.reactive.config.WebFluxConfigurer;
+
+import com.fasterxml.jackson.annotation.JsonInclude.Include;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+
+@Configuration
+public class JsonConfiguration {
+
+    @Bean
+    public JavaTimeModule javatimeModule() {
+        
+        return new JavaTimeModule();
+        
+    }
+
+    @Bean
+    public Jackson2ObjectMapperBuilderCustomizer jsonFormatDateTimes() {
+        
+        return builder -> builder
+                .featuresToDisable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+        
+    }
+
+    @Bean
+    public Jackson2ObjectMapperBuilderCustomizer jsonMinimizeOutput() {
+        
+        return builder -> builder
+                .indentOutput(true)
+                .serializationInclusion(Include.NON_NULL);
+        
+    }
+
+    @Bean
+    public Jackson2JsonEncoder jackson2JsonEncoder(
+            final ObjectMapper mapper) {
+        
+        return new Jackson2JsonEncoder(mapper);
+        
+    }
+
+    @Bean
+    public Jackson2JsonDecoder jackson2JsonDecoder(
+            final ObjectMapper mapper) {
+        
+        return new Jackson2JsonDecoder(mapper);
+        
+    }
+
+    @Bean
+    public WebFluxConfigurer webFluxConfigurer(
+            final Jackson2JsonEncoder encoder,
+            final Jackson2JsonDecoder decoder) {
+        
+        return new WebFluxConfigurer() {
+            @Override
+            public void configureHttpMessageCodecs(
+                    final ServerCodecConfigurer configurer) {
+                
+                configurer.defaultCodecs().jackson2JsonEncoder(encoder);
+                configurer.defaultCodecs().jackson2JsonDecoder(decoder);
+                
+            }
+        };
+
+    }
+
+}
--- business-cockpit/src/main/webapp/src/bootstrap.tsx
+++ business-cockpit/src/main/webapp/src/bootstrap.tsx
@@ -13,14 +13,6 @@ import "@fontsource/roboto/latin-500.css";
 import "@fontsource/roboto/files/roboto-latin-500-normal.woff2";
 import "@fontsource/roboto/files/roboto-latin-500-normal.woff";
 
-// adopt ISO string according to server which is using localdate.
-// therefore we assume that user have the same timezone as the
-// server.
-// eslint-disable-next-line
-Date.prototype.toISOString = function() {
-    return `${this.getFullYear()}-${String(this.getMonth() + 1).padStart(2, '0')}-${String(this.getDate()).padStart(2, '0')}T${String(this.getHours()).padStart(2, '0')}:${String(this.getMinutes()).padStart(2, '0')}:${String(this.getSeconds()).padStart(2, '0')}.${String(this.getMilliseconds()).padStart(3, '0')}`;
-  };
-
 const container = document.getElementById('root');
 const root = createRoot(container!);
 root.render(
