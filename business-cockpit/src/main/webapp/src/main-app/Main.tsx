93fcccf5e57f74be3501ef691c3470e1d1ba68a1
Stephan Pelikan <stephan.pelikan@phactum.at>
2023-06-29 09:45:13 +0200

    Improve welcome screen

--- business-cockpit/src/main/webapp/src/main-app/Main.tsx
+++ business-cockpit/src/main/webapp/src/main-app/Main.tsx
@@ -1,17 +1,33 @@
-import { useLayoutEffect } from 'react';
+import React, { useLayoutEffect } from 'react';
 import { useAppContext } from '../AppContext';
 import { Box } from 'grommet';
-import { Test } from '@vanillabp/bc-shared';
+import { useTranslation } from 'react-i18next';
+import i18n from '../i18n';
 
+i18n.addResources('en', 'main/welcome', {
+    "hello": "Welcome to {{title}}"
+  });
+i18n.addResources('de', 'main/welcome', {
+    "hello": "Willkommen beim {{title}}"
+  });
+  
 const Main = () => {
 
-  const { setAppHeaderTitle } = useAppContext();
-
+  const { state, setAppHeaderTitle } = useAppContext();
+  const { t } = useTranslation('main/welcome');
+  const { t: tTitle } = useTranslation(state.title);
+  
   useLayoutEffect(() => {
     setAppHeaderTitle('app');
   }, [ setAppHeaderTitle ]);
   
-  return <Box>Main: <Test /></Box>;
+  return (
+    <Box
+        fill
+        pad={ { top: 'large' } }
+        align='center'>
+      <Box>{ t('hello', { title: tTitle('title.long') } ) }</Box>
+    </Box>);
 
 }
 
