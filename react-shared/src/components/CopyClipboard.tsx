81fbf682dbe756f8f381a594382f5f17a5074a2f
Stephan Pelikan <stephan.pelikan@phactum.at>
2023-07-27 07:58:15 +0200

    Try to fix rendering problems of CopyClipboard

--- react-shared/src/components/CopyClipboard.tsx
+++ react-shared/src/components/CopyClipboard.tsx
@@ -5,7 +5,7 @@ import { BackgroundType, EdgeSizeType } from 'grommet/utils';
 
 interface CopyClipbardProps {
   content?: string;
-  size?: EdgeSizeType,
+  size?: EdgeSizeType | string,
   color?: string;
   background?: BackgroundType;
 };
@@ -14,15 +14,30 @@ interface CopyClipbardProps {
 const CopyClipboard = ({
   content,
   size = 'medium',
-  background = { color: 'rgba(0, 0, 0, 0.5)' },
+  background = { color: 'rgba(0, 0, 0, 0.45)' },
   color = 'white',
   children,
 }: PropsWithChildren<CopyClipbardProps>) => {
   const targetRef = useRef<HTMLSpanElement>(null);
-  const [over, setOver] = useState(false);
+  const [ over, _setOver ] = useState(false);
+  const hideTimer = React.useRef(0);
   if (!Boolean(content)) {
-    return <>{ children }</>;
+    return <>{children}</>;
   }
+  const setOver = (status: boolean) => {
+    if (Boolean(hideTimer.current)) {
+      window.clearTimeout(hideTimer.current);
+      hideTimer.current = 0;
+    }
+    if (status) {
+      _setOver(status);
+      return;
+    }
+    hideTimer.current = window.setTimeout(() => {
+        _setOver(false);
+        hideTimer.current = 0;
+      }, 200);
+  };
   const copyContent = async (data: string) => {
       try {
         await navigator.clipboard.writeText(data);
@@ -31,12 +46,32 @@ const CopyClipboard = ({
         console.error('Failed to copy: ', err);
       }
     };
+  const sizeFactor =
+    size === "xxsmall"
+      ? 0.3
+      : size === "xsmall"
+      ? 0.4
+      : size === "small"
+      ? 0.5
+      : size === "medium"
+      ? 0.6
+      : size === "large"
+      ? 0.8
+      : size === "xlarge"
+      ? 1
+      : size === "xxlarge"
+      ? 1.2
+      : 0;
+  const iconSize =
+    sizeFactor === 0 || !targetRef.current
+      ? size
+      : `${targetRef.current.clientHeight * sizeFactor}px`;
   return (
       <span
           ref={ targetRef }
           style={ { display: 'inline-block' } }
-          onMouseLeave={ () => setOver(false) }
-          onMouseEnter={ () => setOver(true) }>
+          onMouseLeave={ event => setOver(false) }
+          onMouseEnter={ event => setOver(true) }>
         { children }
         { over && targetRef.current &&
           <Drop
@@ -48,26 +83,11 @@ const CopyClipboard = ({
                 flex={ false }
                 round="xsmall"
                 background={ background }
+                margin={ { left: 'xsmall' } }
                 pad="xsmall">
               <Copy
                   color={ color }
-                  size={
-                    (
-                      targetRef.current.clientHeight *
-                          (size === 'xxsmall'
-                              ? 0.3
-                              : size === 'xsmall'
-                              ? 0.4
-                              : size === 'small'
-                              ? 0.5
-                              : size === 'medium'
-                              ? 0.6
-                              : size === 'large'
-                              ? 0.8
-                              : size === 'xlarge'
-                              ? 1
-                              : 1.2)
-                    ).toString() + 'px' }
+                  size={ size }
                   onClick={ () => copyContent(content!) } />
              </Box>
           </Drop>
