9ca08f78da777a96194716499385a5f1576efdc9
Stephan Pelikan <stephan.pelikan@phactum.at>
2024-02-12 07:53:05 +0100

    Improve loading indicator

--- ui/bc-shared/src/components/LoadingIndicator.tsx
+++ ui/bc-shared/src/components/LoadingIndicator.tsx
@@ -1,10 +1,21 @@
 import React from "react";
 import { Box, Layer, Spinner } from "grommet";
+import styled from "styled-components";
 
 export type ShowLoadingIndicatorFunction = (show: boolean) => void;
 
+// Grommet includes a hidden a-tag to modal layers setting the focus on
+// if the modal content does not catch the focus. This brings a focus indicator
+// at least to Firefox browsers which has to be disabled.
+// see https://github.com/grommet/grommet/issues/7128
+const NoFocusIndicatorLayer = styled(Layer)`
+  a:focus {
+    outline: none;
+  }
+`;
+
 const LoadingIndicator = () => (
-    <Layer
+    <NoFocusIndicatorLayer
         plain
         animate={false}
         background={ { color: 'rgba(0, 0, 0, 0)' } }
@@ -27,6 +38,6 @@ const LoadingIndicator = () => (
           />
         </Box>
       </Box>
-    </Layer>);
+    </NoFocusIndicatorLayer>);
 
 export { LoadingIndicator };
