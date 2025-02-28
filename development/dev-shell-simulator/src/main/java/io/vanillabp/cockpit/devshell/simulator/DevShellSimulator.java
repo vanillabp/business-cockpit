b8a02aeee794275b7159b029900f6c21a5446d05
Tolga Topal <tolga.topal2001@gmail.com>
2025-02-28 14:28:08 +0100

    added excalidraw files, fixed annotations and imports, added new dependency

--- development/dev-shell-simulator/src/main/java/io/vanillabp/cockpit/devshell/simulator/DevShellSimulator.java
+++ development/dev-shell-simulator/src/main/java/io/vanillabp/cockpit/devshell/simulator/DevShellSimulator.java
@@ -2,13 +2,11 @@ package io.vanillabp.cockpit.devshell.simulator;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan(basePackageClasses = BlueprintDevShellSimulator.class)
-public class BlueprintDevShellSimulator {
+public class DevShellSimulator {
 
     public static void main(String[] args) {
-        SpringApplication.run(BlueprintDevShellSimulator.class);
+        SpringApplication.run(DevShellSimulator.class);
     }
 }
