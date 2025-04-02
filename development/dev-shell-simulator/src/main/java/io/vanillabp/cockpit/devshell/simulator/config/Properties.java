8b939cb396a30ec638deb488fb538513e4c75086
Tolga Topal <tolga.topal2001@gmail.com>
2025-04-02 11:52:36 +0200

    removed redundant AutoConfiguration, refactoring

--- development/dev-shell-simulator/src/main/java/io/vanillabp/cockpit/devshell/simulator/config/Properties.java
+++ development/dev-shell-simulator/src/main/java/io/vanillabp/cockpit/devshell/simulator/config/Properties.java
@@ -5,10 +5,8 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.context.annotation.Configuration;
 
 import java.util.List;
-
 
 @NoArgsConstructor
 @AllArgsConstructor
