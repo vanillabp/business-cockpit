d9efec3a85066077d93ce2fbe455521aab665fe8
Stephan Pelikan <stephan.pelikan@phactum.at>
2025-05-12 14:04:46 +0200

    Fix boot order of proxy registration

--- container/src/main/java/io/vanillabp/cockpit/config/MicroserviceProxyConfiguration.java
+++ container/src/main/java/io/vanillabp/cockpit/config/MicroserviceProxyConfiguration.java
@@ -1,12 +1,12 @@
 package io.vanillabp.cockpit.config;
 
+import io.vanillabp.cockpit.util.microserviceproxy.MicroserviceProxyRegistry;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
-import io.vanillabp.cockpit.util.microserviceproxy.MicroserviceProxyRegistry;
+import org.springframework.context.annotation.DependsOn;
 
 @Configuration
 public class MicroserviceProxyConfiguration {
@@ -18,6 +18,8 @@ public class MicroserviceProxyConfiguration {
     private ApplicationEventPublisher applicationEventPublisher;
 
     @Bean
+    // needed because routes causes event to be triggered which seems to bypass auto-configure-before from ChangeSetAutoConfiguration
+    @DependsOn("changesetAutoConfiguration")
     public MicroserviceProxyRegistry microserviceProxyRegistry() {
 
         return new MicroserviceProxyRegistry(
