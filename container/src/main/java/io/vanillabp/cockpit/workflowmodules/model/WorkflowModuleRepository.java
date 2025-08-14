3b2cb12603c4a4aa77116b3f8ccbef02d819ae8a
Laurenz Novotny <laurenz.novotny@wdw-elab.de>
2025-08-14 15:21:04 +0200

    51: Add user permission check to workflow module fetching

--- container/src/main/java/io/vanillabp/cockpit/workflowmodules/model/WorkflowModuleRepository.java
+++ container/src/main/java/io/vanillabp/cockpit/workflowmodules/model/WorkflowModuleRepository.java
@@ -1,8 +1,14 @@
 package io.vanillabp.cockpit.workflowmodules.model;
 
+import java.util.List;
+
 import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
 import org.springframework.stereotype.Repository;
 
+import reactor.core.publisher.Flux;
+
 @Repository
 public interface WorkflowModuleRepository extends ReactiveMongoRepository<WorkflowModule, String> {
+    // IntelliJ thinks we need a Collection of Lists here, but we want intersecting results here
+    Flux<WorkflowModule> findByPermittedRolesNullOrPermittedRolesIn(List<String> permittedRoles);
 }
