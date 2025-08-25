8a6031aa219868c6c0fdd3ed24cf7f96fceb83b1
Martin Greilinger <martin.greilinger@proton.me>
2025-08-25 10:21:56 +0200

    #51 Add abstract workflow modules controller

--- container/src/main/java/io/vanillabp/cockpit/workflowmodules/api/v1/GuiApiController.java
+++ container/src/main/java/io/vanillabp/cockpit/workflowmodules/api/v1/GuiApiController.java
@@ -1,56 +1,19 @@
 package io.vanillabp.cockpit.workflowmodules.api.v1;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
+import io.vanillabp.cockpit.commons.security.usercontext.UserDetails;
+import io.vanillabp.cockpit.workflowmodules.model.WorkflowModule;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Flux;
 
-import io.vanillabp.cockpit.commons.security.usercontext.reactive.ReactiveUserContext;
-import io.vanillabp.cockpit.gui.api.v1.OfficialWorkflowModulesApi;
-import io.vanillabp.cockpit.gui.api.v1.WorkflowModule;
-import io.vanillabp.cockpit.gui.api.v1.WorkflowModules;
-import io.vanillabp.cockpit.workflowmodules.WorkflowModuleService;
-import reactor.core.publisher.Mono;
+import java.util.Collections;
 
 @RestController("workflowModulesGuiApiController")
 @RequestMapping(path = "/gui/api/v1")
-public class GuiApiController implements OfficialWorkflowModulesApi {
-
-    @Autowired
-    private WorkflowModuleService service;
-
-    @Autowired
-    private GuiApiMapper mapper;
-
-    @Autowired
-    private ReactiveUserContext userContext;
+public class GuiApiController extends AbstractWorkflowModulesGuiApiController {
 
     @Override
-    public Mono<ResponseEntity<WorkflowModule>> getWorkflowModule(
-            final String workflowModuleId,
-            final ServerWebExchange exchange) {
-
-        return service
-                .getWorkflowModule(workflowModuleId)
-                .map(mapper::toApi)
-                .map(ResponseEntity::ok)
-                .switchIfEmpty(Mono.just(ResponseEntity.notFound().build()));
-
+    protected Flux<WorkflowModule> getWorkflowMoules(UserDetails userDetails) {
+	return service.getWorkflowModules(Collections.emptyList());
     }
-
-    @Override
-    public Mono<ResponseEntity<WorkflowModules>> getWorkflowModules(
-            final ServerWebExchange exchange) {
-
-        return userContext.getUserLoggedInDetailsAsMono().flatMap(entry ->
-                    service
-                        .getWorkflowModules(entry.getAuthorities())
-                        .map(mapper::toApi)
-                        .collectList()
-                        .map(modules -> new WorkflowModules().modules(modules))
-                        .map(ResponseEntity::ok)
-                );
-    }
-
 }
