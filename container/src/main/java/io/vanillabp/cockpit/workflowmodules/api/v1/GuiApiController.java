772901a80c4a4a4bf1bba8c27e513314ac8957b6
Laurenz Novotny <laurenz.novotny@wdw-elab.de>
2025-08-26 09:48:56 +0200

    #51 Use accessibleToGroups as default for restricting workflow modules

--- container/src/main/java/io/vanillabp/cockpit/workflowmodules/api/v1/GuiApiController.java
+++ container/src/main/java/io/vanillabp/cockpit/workflowmodules/api/v1/GuiApiController.java
@@ -6,14 +6,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Flux;
 
-import java.util.Collections;
-
 @RestController("workflowModulesGuiApiController")
 @RequestMapping(path = "/gui/api/v1")
 public class GuiApiController extends AbstractWorkflowModulesGuiApiController {
 
     @Override
     protected Flux<WorkflowModule> getWorkflowModules(UserDetails userDetails) {
-	    return service.getWorkflowModules(Collections.emptyList());
+	    return service.getWorkflowModules(userDetails.getAuthorities());
     }
 }
