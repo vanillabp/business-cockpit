3a183306444169ecacb54786b31c1d266f857862
Martin Greilinger <martin.greilinger@proton.me>
2026-01-23 13:14:54 +0100

    update webapp to angular 21

--- development/simulator/src/main/webapp-angular/projects/webapp/src/main.ts
+++ development/simulator/src/main/webapp-angular/projects/webapp/src/main.ts
@@ -1,10 +1,8 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-
-import { AppComponent } from './app/app.component';
-
+import { provideHttpClient } from '@angular/common/http';
 import { appConfig } from '@vanillabp/bc-dev-shell-angular';
 import { HeaderComponent, UserTaskFormComponent, WorkflowPageComponent } from '@library';
-import { provideHttpClient } from '@angular/common/http';
+import { AppComponent } from './app/app.component';
 
 bootstrapApplication(AppComponent, appConfig('/official-api/v1', UserTaskFormComponent, WorkflowPageComponent,
     {'header': HeaderComponent}, [provideHttpClient()]))
