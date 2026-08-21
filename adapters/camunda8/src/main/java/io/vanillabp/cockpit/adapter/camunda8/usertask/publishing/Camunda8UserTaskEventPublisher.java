d03abf5991057747e86db5f74e77bed47ee85244
Stephan Pelikan <stephan.pelikan@phactum.at>
2026-08-21 15:16:53 +0200

    Avoid one errornous message causes batch processing to interrupt

--- adapters/camunda8/src/main/java/io/vanillabp/cockpit/adapter/camunda8/usertask/publishing/Camunda8UserTaskEventPublisher.java
+++ adapters/camunda8/src/main/java/io/vanillabp/cockpit/adapter/camunda8/usertask/publishing/Camunda8UserTaskEventPublisher.java
@@ -3,11 +3,15 @@ package io.vanillabp.cockpit.adapter.camunda8.usertask.publishing;
 import io.vanillabp.cockpit.adapter.common.usertask.UserTaskPublishing;
 import java.util.LinkedList;
 import java.util.List;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.event.EventListener;
 import org.springframework.transaction.event.TransactionPhase;
 import org.springframework.transaction.event.TransactionalEventListener;
 
 public class Camunda8UserTaskEventPublisher {
+
+    private static final Logger logger = LoggerFactory.getLogger(Camunda8UserTaskEventPublisher.class);
 
     private final static ThreadLocal<List<UserTaskEvent>> events = ThreadLocal.withInitial(LinkedList::new);
 
@@ -40,7 +44,17 @@ public class Camunda8UserTaskEventPublisher {
                     .get()
                     .stream()
                     .map(UserTaskEvent::getEvent)
-                    .forEach(userTaskPublishing::publish);
+                    // a single failing event must not drop the remaining events of this batch
+                    .forEach(event -> {
+                        try {
+                            userTaskPublishing.publish(event);
+                        } catch (Exception e) {
+                            logger.error("Could not publish user-task event '{}' of user-task '{}'!",
+                                    event.getEventId(),
+                                    event.getUserTaskId(),
+                                    e);
+                        }
+                    });
         } finally {
             
             events.get().clear();
